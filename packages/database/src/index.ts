e6e96c519acc61dd7bf9c681641a7521dabf1faa
pravi4 <pravi4@pravi4s-MacBook-Air.local>
2026-02-27 08:02:42 +0700

    fix(database): use direct imports for schema instead of directory import

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,30 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from './schema/index';
+import * as users from './schema/users';
+import * as chats from './schema/chats';
+import * as messages from './schema/messages';
+import * as messageUsage from './schema/message-usage';
+import * as subscriptions from './schema/subscriptions';
+import * as projects from './schema/projects';
+import * as contextSections from './schema/context-sections';
+import * as usageLogs from './schema/usage-logs';
+import * as modelPricing from './schema/model-pricing';
+import * as adminActions from './schema/admin-actions';
+import * as imageGenerations from './schema/image-generations';
+
+const schema = {
+  ...users,
+  ...chats,
+  ...messages,
+  ...messageUsage,
+  ...subscriptions,
+  ...projects,
+  ...contextSections,
+  ...usageLogs,
+  ...modelPricing,
+  ...adminActions,
+  ...imageGenerations,
+};
 
 export * from './schema/users';
 export * from './schema/chats';
